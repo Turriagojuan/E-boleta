5253ac3de5d5d386d3e2263dd73c270595e423fd
unknown <cristianprietoavella@gmail.com>
2024-10-15 00:05:51 -0500

    Correcion

--- persistencia/SectorDAO.php
+++ persistencia/SectorDAO.php
@@ -4,7 +4,7 @@ class SectorDAO
 {
     // Método para consultar sectores por evento
     public function consultarPorEvento($idEvento){
-        return "SELECT s.idSector, s.nombre, s.precio, se.cantidad 
+        return "SELECT s.idSector, s.nombre, s.precio, s.cantidad 
                 FROM Sector s 
                 INNER JOIN sector_evento se ON s.idSector = se.Sector_idSector 
                 WHERE se.Evento_idEvento = " . $idEvento;
