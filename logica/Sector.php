8093547b7c34d660a9a790b4b78da22633b54e76
Juan Turriago <juanturriagoamaya@gmail.com>
2024-10-15 04:53:35 -0500

    bd actualizada y sector primer cambio

--- logica/Sector.php
+++ logica/Sector.php
@@ -1,60 +1,78 @@
 <?php
 require_once("./persistencia/Conexion.php");
 require_once("./persistencia/SectorDAO.php");
-class Sector{
+
+class Sector {
     private $idSector;
     private $nombre;
     private $precio;
     private $cantidad;
 
-    public function getIdSector(){
+    public function getIdSector() {
         return $this->idSector;
     }
 
-    public function setIdSector($idSector){
+    public function setIdSector($idSector) {
         $this->idSector = $idSector;
     }
-    public function getNombre(){
+
+    public function getNombre() {
         return $this->nombre;
     }
 
-    public function setNombre($nombre){
+    public function setNombre($nombre) {
         $this->nombre = $nombre;
     }
 
-    public function getPrecio(){
+    public function getPrecio() {
         return $this->precio;
     }
 
-    public function setPrecio($precio){
+    public function setPrecio($precio) {
         $this->precio = $precio;
     }
-    public function getCantidad(){
+
+    public function getCantidad() {
         return $this->cantidad;
     }
 
-    public function setCantidad($cantidad){
+    public function setCantidad($cantidad) {
         $this->cantidad = $cantidad;
     }
-    public function __construct($idProducto=0, $nombre="", $precio=0, $cantidad=0){
-        $this -> idSector = $idProducto;
-        $this -> nombre = $nombre;
-        $this -> precio = $precio;
-        $this -> cantidad = $cantidad;
-       }
+
+    public function __construct($idSector = 0, $nombre = "", $precio = 0.0, $cantidad = 0) {
+        $this->idSector = $idSector;
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->cantidad = $cantidad;
+    }
 
     // Método para consultar sectores por evento
-    public function consultarPorEvento($idEvento){
+    public function consultarPorEvento($idEvento) {
         $sectores = array();
         $conexion = new Conexion(); 
         $conexion->abrirConexion();
         $sectorDAO = new SectorDAO();
+        
+        // Asegúrate de que SectorDAO tiene este método implementado
         $conexion->ejecutarConsulta($sectorDAO->consultarPorEvento($idEvento));
+        
         while ($registro = $conexion->siguienteRegistro()) {
-            $sector = new Sector($registro[0], $registro[1], $registro[2]);
+            $sector = new Sector($registro[0], $registro[1], $registro[2], $registro[3]); // Asume que el registro[3] es la cantidad
             array_push($sectores, $sector);
         }
+        
         $conexion->cerrarConexion();
         return $sectores;
     }
+    public function agregar() {
+        $conexion = new Conexion();
+        $conexion->abrirConexion();
+        $sectorDAO = new SectorDAO();
+        $consulta = $sectorDAO->agregarSector($this->nombre, $this->precio, $this->cantidad);
+        $conexion->ejecutarConsulta($consulta);
+        $conexion->cerrarConexion();
+    }
+    
 }
+?>
