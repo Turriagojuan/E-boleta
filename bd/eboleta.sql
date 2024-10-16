eb2825944018338676e33f339c05a61a46322a9d
Juan Turriago <juanturriagoamaya@gmail.com>
2024-10-15 22:13:10 -0500

    Cambio base de datos

--- bd/eboleta.sql
+++ bd/eboleta.sql
@@ -2,9 +2,9 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 15-10-2024 a las 23:59:35
--- Versión del servidor: 10.4.32-MariaDB
+-- Servidor: localhost
+-- Tiempo de generación: 16-10-2024 a las 04:59:00
+-- Versión del servidor: 8.0.39
 -- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -18,10 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `eboleta`
+-- Base de datos: `eboleta2`
 --
-CREATE DATABASE IF NOT EXISTS `eboleta` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `eboleta`;
 
 -- --------------------------------------------------------
 
@@ -30,11 +28,11 @@ USE `eboleta`;
 --
 
 CREATE TABLE `boleta` (
-  `idBoleta` int(11) NOT NULL,
+  `idBoleta` int NOT NULL,
   `nombre_usuario` varchar(45) NOT NULL,
-  `Evento_idEvento` int(11) NOT NULL,
-  `Cliente_idCliente` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `Evento_idEvento` int NOT NULL,
+  `Cliente_idCliente` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Volcado de datos para la tabla `boleta`
@@ -51,9 +49,9 @@ INSERT INTO `boleta` (`idBoleta`, `nombre_usuario`, `Evento_idEvento`, `Cliente_
 --
 
 CREATE TABLE `categoria` (
-  `idCategoria` int(11) NOT NULL,
+  `idCategoria` int NOT NULL,
   `nombre` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Volcado de datos para la tabla `categoria`
@@ -71,13 +69,13 @@ INSERT INTO `categoria` (`idCategoria`, `nombre`) VALUES
 --
 
 CREATE TABLE `cliente` (
-  `idCliente` int(11) NOT NULL,
+  `idCliente` int NOT NULL,
   `nombre` varchar(45) NOT NULL,
   `correo` varchar(45) NOT NULL,
-  `telefono` int(11) NOT NULL,
+  `telefono` int NOT NULL,
   `direccion` varchar(45) NOT NULL,
   `clave` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Volcado de datos para la tabla `cliente`
@@ -94,26 +92,27 @@ INSERT INTO `cliente` (`idCliente`, `nombre`, `correo`, `telefono`, `direccion`,
 --
 
 CREATE TABLE `evento` (
-  `idEvento` int(11) NOT NULL,
+  `idEvento` int NOT NULL,
   `nombre` varchar(45) NOT NULL,
-  `aforo` int(11) NOT NULL,
+  `aforo` int NOT NULL,
   `ciudad` varchar(45) NOT NULL,
   `direccion` varchar(45) NOT NULL,
   `fecha` date NOT NULL,
   `hora` time NOT NULL,
   `descripcion` varchar(300) NOT NULL,
-  `Proveedor_idProveedor` int(11) NOT NULL,
-  `Tipo_evento_idCategoria` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `Proveedor_idProveedor` int NOT NULL,
+  `Tipo_evento_idCategoria` int NOT NULL,
+  `precio` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Volcado de datos para la tabla `evento`
 --
 
-INSERT INTO `evento` (`idEvento`, `nombre`, `aforo`, `ciudad`, `direccion`, `fecha`, `hora`, `descripcion`, `Proveedor_idProveedor`, `Tipo_evento_idCategoria`) VALUES
-(1, 'Concierto Rock', 5000, 'Bogotá', 'Estadio XYZ', '2024-11-01', '19:00:00', 'Un concierto de rock con bandas nacionales.', 1, 1),
-(2, 'Obra de Teatro', 300, 'Medellín', 'Teatro ABC', '2024-12-05', '20:00:00', 'Una obra de teatro clásica.', 2, 2),
-(10, 'Bogotafes', 500, 'Medellin', 'calle de la muelte #4', '2024-10-16', '14:33:00', 'jkbnkjn jknknk kjnk', 1, 2);
+INSERT INTO `evento` (`idEvento`, `nombre`, `aforo`, `ciudad`, `direccion`, `fecha`, `hora`, `descripcion`, `Proveedor_idProveedor`, `Tipo_evento_idCategoria`, `precio`) VALUES
+(1, 'Concierto Rock', 5000, 'Bogotá', 'Estadio XYZ', '2024-11-01', '19:00:00', 'Un concierto de rock con bandas nacionales.', 1, 1, 150000),
+(2, 'Obra de Teatro', 300, 'Medellín', 'Teatro ABC', '2024-12-05', '20:00:00', 'Una obra de teatro clásica.', 2, 2, 50000),
+(10, 'Bogotafes', 500, 'Medellin', 'calle de la muelte #4', '2024-10-16', '14:33:00', 'jkbnkjn jknknk kjnk', 1, 2, 0);
 
 -- --------------------------------------------------------
 
@@ -122,15 +121,15 @@ INSERT INTO `evento` (`idEvento`, `nombre`, `aforo`, `ciudad`, `direccion`, `fec
 --
 
 CREATE TABLE `factura` (
-  `idFactura` int(11) NOT NULL,
+  `idFactura` int NOT NULL,
   `total` float NOT NULL,
   `subtotal` float NOT NULL,
-  `iva` int(11) NOT NULL,
+  `iva` int NOT NULL,
   `fecha` date NOT NULL,
   `hora` time NOT NULL,
-  `Cliente_idCliente` int(11) NOT NULL,
-  `Evento_idEvento` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `Cliente_idCliente` int NOT NULL,
+  `Evento_idEvento` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Volcado de datos para la tabla `factura`
@@ -147,10 +146,10 @@ INSERT INTO `factura` (`idFactura`, `total`, `subtotal`, `iva`, `fecha`, `hora`,
 --
 
 CREATE TABLE `factura_boleta` (
-  `Boleta_idBoleta` int(11) NOT NULL,
-  `Factura_idFactura` int(11) NOT NULL,
-  `cantidad` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `Boleta_idBoleta` int NOT NULL,
+  `Factura_idFactura` int NOT NULL,
+  `cantidad` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Volcado de datos para la tabla `factura_boleta`
@@ -167,13 +166,13 @@ INSERT INTO `factura_boleta` (`Boleta_idBoleta`, `Factura_idFactura`, `cantidad`
 --
 
 CREATE TABLE `proveedor` (
-  `idProveedor` int(11) NOT NULL,
+  `idProveedor` int NOT NULL,
   `nombre` varchar(45) NOT NULL,
   `correo` varchar(45) NOT NULL,
-  `telefono` int(11) NOT NULL,
+  `telefono` int NOT NULL,
   `direccion` varchar(45) NOT NULL,
   `clave` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Volcado de datos para la tabla `proveedor`
@@ -182,48 +181,6 @@ CREATE TABLE `proveedor` (
 INSERT INTO `proveedor` (`idProveedor`, `nombre`, `correo`, `telefono`, `direccion`, `clave`) VALUES
 (1, 'Proveedor 1', 'contacto@proveedor1.com', 123456789, 'Calle 123', 'eb52fc9a4b3a81a2000a9e774d5aa515'),
 (2, 'Proveedor 2', 'contacto@proveedor2.com', 987654321, 'Avenida 456', 'b984fe77863037ddeb9be2ad7dfb246e');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `sector`
---
-
-CREATE TABLE `sector` (
-  `idSector` int(11) NOT NULL,
-  `nombre` varchar(45) NOT NULL,
-  `precio` float NOT NULL,
-  `cantidad` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Volcado de datos para la tabla `sector`
---
-
-INSERT INTO `sector` (`idSector`, `nombre`, `precio`, `cantidad`) VALUES
-(1, 'VIP', 150000, 100),
-(2, 'General', 50000, 4000),
-(3, 'Preferencial', 100000, 500);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `sector_evento`
---
-
-CREATE TABLE `sector_evento` (
-  `Sector_idSector` int(11) NOT NULL,
-  `Evento_idEvento` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-
---
--- Volcado de datos para la tabla `sector_evento`
---
-
-INSERT INTO `sector_evento` (`Sector_idSector`, `Evento_idEvento`) VALUES
-(1, 1),
-(2, 1),
-(3, 2);
 
 --
 -- Índices para tablas volcadas
@@ -279,19 +236,6 @@ ALTER TABLE `proveedor`
   ADD PRIMARY KEY (`idProveedor`);
 
 --
--- Indices de la tabla `sector`
---
-ALTER TABLE `sector`
-  ADD PRIMARY KEY (`idSector`);
-
---
--- Indices de la tabla `sector_evento`
---
-ALTER TABLE `sector_evento`
-  ADD PRIMARY KEY (`Sector_idSector`,`Evento_idEvento`),
-  ADD KEY `fk_Sector_has_Evento_Evento1` (`Evento_idEvento`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -299,7 +243,7 @@ ALTER TABLE `sector_evento`
 -- AUTO_INCREMENT de la tabla `evento`
 --
 ALTER TABLE `evento`
-  MODIFY `idEvento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `idEvento` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Restricciones para tablas volcadas
@@ -309,36 +253,29 @@ ALTER TABLE `evento`
 -- Filtros para la tabla `boleta`
 --
 ALTER TABLE `boleta`
-  ADD CONSTRAINT `fk_Boleta_Cliente1` FOREIGN KEY (`Cliente_idCliente`) REFERENCES `cliente` (`idCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Boleta_Evento1` FOREIGN KEY (`Evento_idEvento`) REFERENCES `evento` (`idEvento`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Boleta_Cliente1` FOREIGN KEY (`Cliente_idCliente`) REFERENCES `cliente` (`idCliente`),
+  ADD CONSTRAINT `fk_Boleta_Evento1` FOREIGN KEY (`Evento_idEvento`) REFERENCES `evento` (`idEvento`);
 
 --
 -- Filtros para la tabla `evento`
 --
 ALTER TABLE `evento`
-  ADD CONSTRAINT `fk_Evento_Proveedor` FOREIGN KEY (`Proveedor_idProveedor`) REFERENCES `proveedor` (`idProveedor`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Evento_Tipo_evento1` FOREIGN KEY (`Tipo_evento_idCategoria`) REFERENCES `categoria` (`idCategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Evento_Proveedor` FOREIGN KEY (`Proveedor_idProveedor`) REFERENCES `proveedor` (`idProveedor`),
+  ADD CONSTRAINT `fk_Evento_Tipo_evento1` FOREIGN KEY (`Tipo_evento_idCategoria`) REFERENCES `categoria` (`idCategoria`);
 
 --
 -- Filtros para la tabla `factura`
 --
 ALTER TABLE `factura`
-  ADD CONSTRAINT `fk_Factura_Cliente1` FOREIGN KEY (`Cliente_idCliente`) REFERENCES `cliente` (`idCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Factura_Evento1` FOREIGN KEY (`Evento_idEvento`) REFERENCES `evento` (`idEvento`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Factura_Cliente1` FOREIGN KEY (`Cliente_idCliente`) REFERENCES `cliente` (`idCliente`),
+  ADD CONSTRAINT `fk_Factura_Evento1` FOREIGN KEY (`Evento_idEvento`) REFERENCES `evento` (`idEvento`);
 
 --
 -- Filtros para la tabla `factura_boleta`
 --
 ALTER TABLE `factura_boleta`
-  ADD CONSTRAINT `fk_Factura_Boleta_Boleta1` FOREIGN KEY (`Boleta_idBoleta`) REFERENCES `boleta` (`idBoleta`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Factura_Boleta_Factura1` FOREIGN KEY (`Factura_idFactura`) REFERENCES `factura` (`idFactura`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
---
--- Filtros para la tabla `sector_evento`
---
-ALTER TABLE `sector_evento`
-  ADD CONSTRAINT `fk_Sector_has_Evento_Evento1` FOREIGN KEY (`Evento_idEvento`) REFERENCES `evento` (`idEvento`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Sector_has_Evento_Sector1` FOREIGN KEY (`Sector_idSector`) REFERENCES `sector` (`idSector`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Factura_Boleta_Boleta1` FOREIGN KEY (`Boleta_idBoleta`) REFERENCES `boleta` (`idBoleta`),
+  ADD CONSTRAINT `fk_Factura_Boleta_Factura1` FOREIGN KEY (`Factura_idFactura`) REFERENCES `factura` (`idFactura`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
